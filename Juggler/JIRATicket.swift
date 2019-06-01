33b4a2d0bc5c7a87f762d7f55705ed244db93513
Tamas Lustyik <elveestei@gmail.com>
2019-06-01 20:44:51 +0200

    Implemented URL provider for JIRA tickets

---
+++ Juggler/JIRATicket.swift
@@ -0,0 +1,55 @@
+//
+//  JIRATicket.swift
+//  Juggler
+//
+//  Created by Tamas Lustyik on 2019. 06. 01..
+//  Copyright © 2019. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+struct JIRATicket: Ticket {
+    var kind: TicketKind { return .jira }
+    let id: String
+    let title: String?
+    let url: URL
+}
+
+class JIRATicketURLProvider {
+    private let userDefaults: UserDefaults
+    private static let jiraBaseURLKey = "JIRABaseURL"
+    
+    var baseURL: URL {
+        get {
+            guard var baseURLStr = userDefaults.string(forKey: JIRATicketURLProvider.jiraBaseURLKey) else {
+                return URL(string: "https://example.com/")!
+            }
+            
+            if !baseURLStr.hasPrefix("https://") && !baseURLStr.hasPrefix("http://") {
+                baseURLStr = "https://" + baseURLStr
+            }
+            
+            guard let url = URL(string: baseURLStr) else {
+                return URL(string: "https://example.com/")!
+            }
+
+            return url
+        }
+        set {
+            if newValue.scheme != "https" && newValue.scheme != "http" {
+                return
+            }
+            
+            userDefaults.set(newValue.absoluteString, forKey: JIRATicketURLProvider.jiraBaseURLKey)
+        }
+    }
+    
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    func url(for ticketID: String) -> URL {
+        return baseURL.appendingPathComponent("browse").appendingPathComponent(ticketID)
+    }
+}
+
