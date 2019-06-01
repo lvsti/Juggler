da79f5915ece322ccbd2c29c75056a976852f08b
Tamas Lustyik <elveestei@gmail.com>
2019-06-01 17:25:00 +0200

    Initial commit

---
+++ Juggler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Juggler
+//
+//  Created by Tamas Lustyik on 2019. 06. 01..
+//  Copyright © 2019. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
